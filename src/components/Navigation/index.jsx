33ca69c17a0591fe449c963965f5fd20c5cb0ba8
Davide Baldi <d.baldi88@gmail.com>
2024-04-10 13:48:17 +0200

    hamburger menu add

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,10 +1,19 @@
 import styles from './Navigation.module.css'
 
 const Navigation = () => {
+
+  const handleClickIconMenu = () => {
+    console.log("Clicked")
+  }
     return (
         <>
           <nav className={styles.navigation}>
-          
+          <input id={styles.toggle1} type="checkbox" />
+            <label className={styles.mobileImg} onClick={handleClickIconMenu} for={styles.toggle1}>
+              <div class={styles.top}></div>
+              <div class={styles.meat}></div>
+              <div class={styles.bottom}></div>
+            </label>
           </nav>
         </>
     )
