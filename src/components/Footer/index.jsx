bb8528e6f595389f3154b4ecb0cb8570e51ae614
Davide Baldi <d.baldi88@gmail.com>
2024-04-10 13:35:20 +0200

    basic structure created

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <>
+          <footer className={styles.footer}>
+            <div>@Davide Baldi 20/04/2024</div>
+          </footer>
+        </>
+    )
+};
+
+export default Footer
---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,13 @@
+import styles from './Navigation.module.css'
+
+const Navigation = () => {
+    return (
+        <>
+          <nav className={styles.navigation}>
+
+          </nav>
+        </>
+    )
+};
+
+export default Navigation;
